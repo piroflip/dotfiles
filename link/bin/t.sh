1eb0f15eea0ee8f58ceb7628aca593e56595dcca
Alexandr Kotov <kot@ya.ru>
2015-06-04 20:25:02 +0300

    add bin dir + bspwm changes

---
+++ link/bin/t.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SESSION=main
+tmux="tmux "
+
+# if the session is already running, just attach to it.
+$tmux has-session -t $SESSION
+if [ $? -eq 0 ]; then
+  echo "Session $SESSION already exists. Attaching."
+  sleep 1
+  $tmux attach -t $SESSION
+  exit 0;
+fi
+# create a new session, named $SESSION, and detach from it
+$tmux new-session -d -s $SESSION
+
+$tmux new-window    -t $SESSION:0 -k -n i686 'sudo chroot /opt/arch32 sudo su piroflip'
+$tmux new-window    -t $SESSION:1 -k -n vm   'cd /mnt/disk/devel/fpsu/asyn/access/makefres/x86;mc'
+$tmux new-window    -t $SESSION:3
+
+$tmux select-window -t $SESSION:3
+$tmux attach -t $SESSION
